6e1a337c0ab9c494b50de0c1b72ce48f9fce7382
akshaynayak <akshaynayak2012@gmail.com>
2016-02-13 11:38:06 -0500

    added junk 2

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,6 @@
 class Admin < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
 
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ActiveRecord::Base
+  validates :course_number, :presence => true
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates:
 end
